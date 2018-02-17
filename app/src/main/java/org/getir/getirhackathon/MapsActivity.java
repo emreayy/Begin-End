5b3d43ca7eb1c0fd539175998a6cc1260ed12b8c
metinsaritas <metinsaritas2@gmail.com>
2018-02-18 00:16:05 +0300

    Main page bug fixed

--- app/src/main/java/org/getir/getirhackathon/MapsActivity.java
+++ app/src/main/java/org/getir/getirhackathon/MapsActivity.java
@@ -38,6 +38,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
